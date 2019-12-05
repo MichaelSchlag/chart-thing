e4fc1571e8a69c9bb0237806d7bf91f816cb8beb
MichaelSchlag <michaeldschlag@gmail.com>
2019-12-05 03:22:04 -0600

    added the secret decoder function. basically, things just weren't working the way i expected them to for hours, and i got so mad that i made the "secretString". it is a text input that saves the position of every chartItem and i am gonna hide it, and uhh, yeah so the controller picks up this gross string and i made the function which parses it and saves the spot of every item. just very happy it works. ok gonna make it so when you add a chartItem it also saves other chartItem's spots

--- src/main/java/com/example/chartthing/Controllers/ChartController.java
+++ src/main/java/com/example/chartthing/Controllers/ChartController.java
@@ -8,13 +8,12 @@ import com.example.chartthing.Models.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,6 +23,7 @@ import java.util.List;
 
 
 @Controller
+@ControllerAdvice
 @RequestMapping(value = "")
 public class ChartController {
 
@@ -42,11 +42,23 @@ public class ChartController {
         return "Chart/index";
     }
 
-    @RequestMapping(value = "new")
+    @RequestMapping(value = "new", method = RequestMethod.GET)
     public String newChart(Model model){
         model.addAttribute("items", chartItemDao.findAll());
         model.addAttribute("title", "New Chart");
         return "Chart/new-chart";
+    }
+
+    @RequestMapping(value = "new", method = RequestMethod.POST)
+    public String postNewChart(Model model, Errors errors, @ModelAttribute @Valid Chart newChart){
+
+        if(errors.hasErrors()){
+            model.addAttribute("items", chartItemDao.findAll());
+            model.addAttribute("title", "New Chart");
+            return "Chart/new-chart";
+        }
+
+        return "test-success";
     }
 
     @RequestMapping(value = "upload")
@@ -63,8 +75,11 @@ public class ChartController {
             }
         }
         ChartItem newChartItem = new ChartItem(0, 0, "" + files[0].getOriginalFilename());
+
         System.out.println(newChartItem);
         chartItemDao.save(newChartItem);
+
+//        secretDecoder(secret);
 
         model.addAttribute("items", chartItemDao.findAll());
         model.addAttribute("title", "New Chart");
@@ -74,17 +89,55 @@ public class ChartController {
     }
 //, method = RequestMethod.POST
     @RequestMapping(value = "delete")
-    public String delete(Model model, @RequestParam("del_id") String del_id_string){
+    public String delete(Model model, @RequestParam("secretStringDel") String secretString, @RequestParam("del_id") String del_id_string){
 
         Integer del_id = Integer.parseInt(del_id_string);
         System.out.println(del_id);
 
         chartItemDao.deleteById(del_id);
 
+        secretDecoder(secretString);
+
         model.addAttribute("items", chartItemDao.findAll());
         model.addAttribute("title", "New Chart");
 
         return "redirect:/new";
+    }
+
+    public void secretDecoder(String secret){
+        System.out.println(secret);
+        char[] letter = secret.toCharArray();
+        int c = 0;
+        while(c < secret.length()) {
+            String c_id = "";
+            String c_x = "";
+            String c_y = "";
+            while (letter[c] != ',') {
+                c_id += letter[c];
+                c++;
+            }
+            int id = Integer.parseInt(c_id);
+            c++;
+            while (letter[c] != ',') {
+                c_x += letter[c];
+                c++;
+            }
+            int x = Integer.parseInt(c_x);
+            c++;
+            while (letter[c] != '!') {
+                c_y += letter[c];
+                c++;
+            }
+            int y = Integer.parseInt(c_y);
+            c++;
+            if (chartItemDao.existsById(id)) {
+                ChartItem temp_item = chartItemDao.findById(id).get();
+                System.out.println(temp_item);
+                temp_item.setX(x);
+                temp_item.setY(y);
+                chartItemDao.save(temp_item);
+            }
+        }
     }
 
 }
